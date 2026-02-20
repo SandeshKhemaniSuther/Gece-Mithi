4557129f41b7d73e0116fd5327b39effd1b0f1f8
SandeshKhemaniSuther <sandesh.suther@gmail.com>
2026-02-20 18:13:59 +0500

    update

--- src/components/aboutus.jsx
+++ src/components/aboutus.jsx
@@ -131,7 +131,7 @@ const AboutUs = () => {
 const facultyData = [
     { id: 1, name: "Late Sir. Ganesh Mal", role: "Assistant Professor", duration: "2005 - 2008" },
     { id: 2, name: "Sir. Tikam Das", role: "Assistant Professor", duration: "2018 - 2022" },
-    { id: 3, name: "Sir. Abdul Wahid Samo", role: "Lecturer", duration: "2012 - 2018" },
+    { id: 3, name: "Sir. Abdul Wahid Samoon", role: "Lecturer", duration: "2012 - 2018" },
     { id: 4, name: "Sir. Yousif Channa", role: "Lecturer", duration: "2012 - 2018" },
     { id: 5, name: "Sir. Nihal Chand", role: "Lecturer", duration: "2012 - 2018" },
     { id: 6, name: "Sir. Mian Bux Kapri", role: "Lecturer", duration: "2012 - 2018" },
@@ -232,7 +232,14 @@ const facultyData = [
             members: [
                 { name: "Miss. Marvi Junejo" }
             ]
+             },
+        {
+            batch: "Batch 2k25",
+            members: [
+                { name: "Mr. Bhaweesh Gul" }
+            ]
         }
+
     ];
 
     // --- SLIDER TIMER ---
